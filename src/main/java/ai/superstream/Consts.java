c0c9a3da921d57c508a5affc38fb38667e1e7276
shay23b <shay@memphis.dev>
2024-05-05 17:18:18 +0300

    fix

--- src/main/java/ai/superstream/Consts.java
+++ src/main/java/ai/superstream/Consts.java
@@ -1,7 +1,7 @@
 package ai.superstream;
 
 public class Consts {
-    public static final String sdkVersion  = "1.0.0";
+    public static final String sdkVersion  = "1.0.2";
     public static final String clientReconnectionUpdateSubject  = "internal.clientReconnectionUpdate";
     public static final String clientTypeUpdateSubject  = "internal.clientTypeUpdate";
     public static final String clientRegisterSubject  = "internal.registerClient";
