a545ca21dde6217ed9727f6c12c89de36a7189db
Shay Bratslavsky <shay@superstream.ai>
2024-06-16 15:36:42 +0300

    1.0.10 (#70)
    * update version
    
    * add tags to clients errors
    
    * fix handle error + client updates every 10 minutes (#69)
    
    ---------
    
    Co-authored-by: idanasulinStrech <idan@memphis.dev>

--- src/main/java/ai/superstream/Superstream.java
+++ src/main/java/ai/superstream/Superstream.java
@@ -418,7 +418,7 @@ public class Superstream {
             } catch (Exception e) {
                 handleError("reportClientsUpdate: " + e.getMessage());
             }
-        }, 0, 2, TimeUnit.MINUTES);
+        }, 0, 10, TimeUnit.MINUTES);
     }
 
     public static Map<String, Integer[]> convertMap(Map<String, Set<Integer>> topicPartitions) {
@@ -625,13 +625,17 @@ public class Superstream {
 
     public void handleError(String msg) {
         if (brokerConnection != null) {
+            Map<String, String> envVars = System.getenv();
+            String tags = envVars.get("SUPERSTREAM_TAGS");
+            if (tags == null) {
+                tags = "";
+            }
             if (clientID == 0) {
-                String message = String.format("[sdk: java][version: %s][connectionID: %s] %s", Consts.sdkVersion,
-                        this.natsConnectionID, msg);
+                String message = String.format("[sdk: java][version: %s][tags: %s] %s", Consts.sdkVersion, tags, msg);
                 brokerConnection.publish(Consts.superstreamErrorSubject, message.getBytes(StandardCharsets.UTF_8));
             } else {
-                String message = String.format("[account name: %s][clientID: %d][sdk: java][version: %s] %s",
-                        accountName, clientID, Consts.sdkVersion, msg);
+                String message = String.format("[clientID: %d][sdk: java][version: %s][tags: %s] %s",
+                        clientID, Consts.sdkVersion, tags, msg);
                 brokerConnection.publish(Consts.superstreamErrorSubject, message.getBytes(StandardCharsets.UTF_8));
             }
         }
